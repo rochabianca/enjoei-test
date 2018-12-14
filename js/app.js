f157c325edbe1827a7a90b02e82e3fb1be38f1f6
Bianca <bianca.rocha1711@gmail.com>
2018-12-13 21:49:53 -0300

    getting resume and total for page

--- js/app.js
+++ js/app.js
@@ -4,6 +4,8 @@ jQuery.get('/api/checkouts/:checkoutId', function(data) {
   console.log(data);
   $("[data='product-image']").prop('src', data.product.image); // change the image for the image of the server
   loadCoupons(data.checkout.availableCoupons);
+  loadResume(data.product, data.checkout);
+  getTotal();
 });
 
 function loadCoupons(coupons) {
@@ -16,7 +18,23 @@ function loadCoupons(coupons) {
         coupons[i].discount.toFixed(2) +
         "</span></div><input type='radio' name='radio' value='" +
         coupons[i].id +
-        "' /><span class='checkbox__checkmark' /></label></li>"
+        "' /><span class='checkbox__checkmark' ></span></label></li>"
     );
   }
+}
+
+function loadResume(product, checkout) {
+  $("[data='product-value']").text('R$ ' + product.price.toFixed(2));
+  $("[data='shippment-value']").text('R$ ' + checkout.shippingPrice.toFixed(2));
+}
+function getTotal() {
+  var initialValue = getCurrency($("[data='product-value']").text());
+  var couponValue = getCurrency($("[data='coupon-value']").text());
+  var shippmentValue = getCurrency($("[data='shippment-value']").text());
+  var totalValue = initialValue + shippmentValue - couponValue;
+  $("[data='total-value']").text('R$ ' + totalValue);
+}
+
+function getCurrency(currency) {
+  return Number(currency.replace(/[^0-9.-]+/g, ''));
 }
